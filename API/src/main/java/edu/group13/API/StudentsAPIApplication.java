c6eaa4eff8d7b384865924042e9819336971bf6f
David Rowland <darowlan@email.uark.edu>
2020-02-17 21:06:38 -0600

    Updated CORS policy so clients can call API

--- API/src/main/java/edu/group13/API/StudentsAPIApplication.java
+++ API/src/main/java/edu/group13/API/StudentsAPIApplication.java
@@ -2,12 +2,29 @@ package edu.group13.API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class StudentsAPIApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StudentsAPIApplication.class, args);
+    }
+
+    //TODO: Update allowedOrigins with the url of our clients to secure from CORS related attacks
+    //The addresses of the host we are allowing to access the API should be added to "allowedOrigins".
+    //The wildcard means that any client on the web can access our API.
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/all").allowedOrigins("*");
+                registry.addMapping("/line").allowedOrigins("*");
+            }
+        };
     }
 
 }
