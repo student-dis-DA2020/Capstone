4a87fa12a4c347b6efd5c667b5e3b1305daa9910
Tomas-Stevens <tomas.stevens1996@gmail.com>
2020-02-22 00:43:36 -0600

    Update students.js
    added drop down menu for later use

--- Website/school-dismissal-app/src/components/students.js
+++ Website/school-dismissal-app/src/components/students.js
@@ -12,11 +12,26 @@ const Students = ({ students }) => {
             <p class="card-text">Teacher: {student.teacher}</p>
             <p class="card-text">Parent: {student.guardians[0]}</p>
             <p class="card-text">Mode: {student.mode}</p>
+
+          <p>
+            <label>Select list</label>
+             <select id = "myList">
+               <option value = "1">one</option>
+               <option value = "2">two</option>
+               <option value = "3">three</option>
+               <option value = "4">four</option>
+             </select>
+          </p>
+
+
           </div>
         </div>
       ))}
     </div>
+    
   )
+  
 };
+
 
 export default Students
