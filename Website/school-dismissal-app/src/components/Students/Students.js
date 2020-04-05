c07db2f02f49f8067f5e1e7b990c5c5cb2b35f93
Tomas Stevens <39743395+tomas-stevens@users.noreply.github.com>
2020-04-05 01:01:02 -0500

    student.js updated
    updated look

--- Website/school-dismissal-app/src/components/Students/Students.js
+++ Website/school-dismissal-app/src/components/Students/Students.js
@@ -6,27 +6,36 @@ const Students = ({ students }) => {
     <div>
       <center><h1>Student List</h1></center>
       {students.map((student) => (
+        <div class="card-deck">
         <div class="card">
           <div class="card-body">
             <h5 class="card-title">{student._id}</h5>
             <h6 class="card-subtitle mb-2 text-muted">{student.name}</h6>
-            <p class="card-text">Teacher: {student.teacher}</p>
-            <p class="card-text">
-            <label>Parents: </label>
-             <select id = "myList">
-              <option value = "1">{student.guardians[0]}</option>
-              <option value = "2">{student.guardians[1]}</option>
-             </select>
-            </p>
-            <p class="card-text">Mode: {student.mode}</p>
-            <p class="card-text">
-            <label>parents car's: </label>
-             <select id = "myList">
-               <option value = "1">{student.cars[0]}</option>
-             </select>
-            </p>
+            <li class="list-group-item">Teacher: {student.teacher} </li>
+            <li class="list-group-item"> Bus number: {student.bus}</li>
+            <li class="list-group-item"> Class Room: {student.room}</li>
+            <li class="list-group-item"> Students Grade: {student.grade}</li>
+            <li class="list-group-item"> Mode of Transportation: {student.mode}</li>
+            <li class="list-group-item">
+              <p class="card-text">
+              <label>Parents: </label>
+                <select id = "myList">
+                  <option value = "1">{student.guardians[0]}</option>
+                  <option value = "2">{student.guardians[1]}</option>
+                </select>
+              </p>
+            </li>
+            <li class="list-group-item">Mode: {student.mode}</li>
+            <li class="list-group-item">
+              <p class="card-text">
+                <label>parents car's: </label>
+                <select id = "myList">
+                  <option value = "1">{student.cars[0]}</option>
+                </select>
+              </p>
+            </li>
 
-
+            </div>
           </div>
         </div>
       ))}
